9647ac6f44be4b76173f74c532498748cbdbe910
annalavrenkoepfl <anna.lavrenko@epfl.ch>
2024-12-05 17:55:03 +0100

    fix

--- data/Cleaning.py
+++ data/Cleaning.py
@@ -14,18 +14,33 @@ result = data_specific.groupby(['WarName', 'Side']).agg(
 
 data = [
     ["Gaza-Israel conflict", "Israel", 1, 2006, -7, 42763],
+    ["Gaza-Israel conflict", "Palestine", 2, 2006, -7, 42763],
     ["Russo-Georgian War", ['Russia', 'South Ossetia', 'Abkhazia'], 1, 2008, 2008, 734],
     ["Russo-Georgian War", "Georgia", 2, 2008, 2008, 734],
     ["2011 military intervention in Libya Part of the First Libyan Civil War", ['NATO', 'Qatar', 'Sweden', 'United Arab Emirates'], 1, 2011, 2011, 238],
     ["2011 military intervention in Libya Part of the First Libyan Civil War", "Libya", 2, 2011, 2011, 238],
-    ["Heglig Crisis", "Sudan", 1, 2013, -7, 900],
-    ["Heglig Crisis", "South Sudan", 2, 2013, -7, 900]
+    ["Heglig Crisis", "Sudan", 1, 2012, 2012, 900],
+    ["Heglig Crisis", "South Sudan", 2, 2012, 2012, 900]
 ]
 print(result.columns)
 df_new = pd.DataFrame(data, columns=result.columns)
 final_df = pd.concat([result, df_new], ignore_index=True)
 final_df = final_df[final_df["StartYear"] >= 1894]
 print(final_df)
+print(final_df.tail())
 
-print(final_df.head())
+final_df.set_index("WarName", inplace=True)
+counts = []
+for war in final_df.index:
+    start_year = final_df.loc[war, "StartYear"].min()
+    end_year = final_df.loc[war, "EndYear"].max()
+    print(start_year, end_year)
+    movies_war = data[data['Year'].between(start_year-2, end_year+2)]
+    print(f"Movies about {war}: {len(movies_war)}")
+    counts.append(len(movies_war))
+final_df["Movies"] = counts
+final_df=final_df[final_df["Movies"]>500]
+data = pd.read_csv("data/movies_with_summaries.csv")
+
+print(final_df.tail())
 print(final_df.shape)
