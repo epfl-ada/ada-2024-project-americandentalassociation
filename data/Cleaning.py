bbec486871d7f5ede65f915d72444d7bc5dc7581
annalavrenkoepfl <anna.lavrenko@epfl.ch>
2024-12-05 11:05:51 +0100

    Update Cleaning.py

--- data/Cleaning.py
+++ data/Cleaning.py
@@ -19,12 +19,12 @@ data = [
     ["2011 military intervention in Libya Part of the First Libyan Civil War", ['NATO', 'Qatar', 'Sweden', 'United Arab Emirates'], 1, 2011, 2011, 238],
     ["2011 military intervention in Libya Part of the First Libyan Civil War", "Libya", 2, 2011, 2011, 238],
     ["Heglig Crisis", "Sudan", 1, 2013, -7, 900],
-    ["Heglig Crisis", "South Sudan", 2, 2013, -7, 900],
+    ["Heglig Crisis", "South Sudan", 2, 2013, -7, 900]
 ]
-
 print(result.columns)
 df_new = pd.DataFrame(data, columns=result.columns)
 final_df = pd.concat([result, df_new], ignore_index=True)
+final_df = final_df[final_df["StartYear"] >= 1894]
 print(final_df)
 
 print(final_df.head())
